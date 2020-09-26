edf399f9f15877c8c68dc9b3e3aeae06fe50aa7e
Tomislav Borić <tomislav.boric@gmail.com>
2020-09-27 01:45:51 +0200

    # Initial commit #

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi! I am Front-end developer from Zagreb, Croatia.<br />
+        Having 11 years of experience in development.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
